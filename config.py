38422a26588c3815c2b218edcac93f87c80a013f
denki307 <mbtdenki@gmail.com>
2025-03-30 19:22:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,7 +89,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/62c76ac2095332a0ede75.jpg"
+    "START_IMG_URL", "https://graph.org/file/50aee8db93fbae6c8ab17-b158b495b18917b837.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/4f59fb748e1990acfa297.jpg"
