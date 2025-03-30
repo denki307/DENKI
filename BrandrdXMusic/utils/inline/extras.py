62c122098182b6f22885fa24c8210912d479fc25
denki307 <mbtdenki@gmail.com>
2025-03-30 20:33:50 +0530

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/JALLI_KATTU_KALA143"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
